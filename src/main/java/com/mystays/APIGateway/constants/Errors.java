697e89d8128ce76d57e0282893346934b4d337f3
nshibu <v-nshibu@expedia.com>
2022-11-10 10:07:22 +0530

    [UP][TECH-504] Exception handling

--- src/main/java/com/mystays/APIGateway/constants/Errors.java
+++ src/main/java/com/mystays/APIGateway/constants/Errors.java
@@ -1,7 +1,7 @@
 package com.mystays.APIGateway.constants;
 
 public enum Errors {
-    DEFAULT(1000, "Authentication failure."),
+    DEFAULT(1000, "Authentication Service unavailable"),
     HOTEL_SEARCH_SERVICE(1002, "Hotel Search Service unavailable"),
     ACCOUNT_SERVICE(1003, "Account Service unavailable"),
     GOOGLE_API(1004, "Google API unavailable");
