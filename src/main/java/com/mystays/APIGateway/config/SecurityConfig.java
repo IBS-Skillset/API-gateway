de8dc4518d5466e32d3590ffde520634be88193e
bkalarikkal <v-bkalarikkal@Traveldoo.com>
2023-04-24 14:18:31 +0530

    [AWS] - reverting

--- src/main/java/com/mystays/APIGateway/config/SecurityConfig.java
+++ src/main/java/com/mystays/APIGateway/config/SecurityConfig.java
@@ -15,9 +15,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
-        /*http.csrf().disable().authorizeExchange()
-                .pathMatchers("/api-gateway/**").permitAll()
-                //.pathMatchers("/api-gateway/**").authenticated()
+        http.csrf().disable().authorizeExchange()
+                .pathMatchers("/api-gateway/account/api/signup").permitAll()
+                .pathMatchers("/api-gateway/**").authenticated()
                 //.anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
@@ -29,7 +29,7 @@ public class SecurityConfig {
             configuration.setAllowedMethods(List.of("*"));
             configuration.setAllowedHeaders(List.of("*"));
             return configuration;
-        });*/
+        });
 
         return http.build();
 
